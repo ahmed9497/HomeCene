d7862ca539de66a92c0ec188761a14d6a1b1e8fc
Ahmed Khalil <ahmed@hypt.app>
2025-02-14 10:29:39 +0500

    config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // trailingSlash:true,
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ['images.pexels.com','vinova-furstore.myshopify.com','res.cloudinary.com'],
   },
