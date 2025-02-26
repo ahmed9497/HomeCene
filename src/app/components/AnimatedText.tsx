d41db97daa55f73a46277a5235009e9149a46117
Ahmed Khalil <ahmed@hypt.app>
2025-02-26 11:51:22 +0500

    drawer

---
+++ src/app/components/AnimatedText.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const messages = [
+  "20% OFF on everything",
+  "Ramadan Sale",
+  "Pay in installments with Tabby",
+];
+
+export default function AnimatedText() {
+  const [index, setIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false); // Start fade-out
+      setTimeout(() => {
+        setIndex((prev) => (prev + 1) % messages.length); // Change message
+        setFade(true); // Fade-in new message
+      }, 500); // Delay before switching message
+    }, 3000); // Change message every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="h-8 rounded-sm bg-red-900 flex justify-center items-center overflow-hidden">
+      <p
+        className={`transition-all duration-500 transform ${
+          fade ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-5"
+        } text-sm  text-white`}
+      >
+        {messages[index]}
+      </p>
+    </div>
+  );
+}
--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -2,12 +2,14 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaArrowRight, FaShoppingCart } from "react-icons/fa";
 import { FaCross, FaMinus, FaPlus } from "react-icons/fa6";
 import { IoCartOutline, IoClose } from "react-icons/io5";
 import { useCart } from "../context/CartContext";
 import Image from "next/image";
 import Link from "next/link";
+import { MdDeleteForever } from "react-icons/md";
+import AnimatedText from "./AnimatedText";
 
 const CartDrawer = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -63,182 +65,184 @@ const CartDrawer = () => {
         </button>
 
         <h2 className="text-xl font-semibold mb-4">My Cart</h2>
-
+          <AnimatedText/>
         {/* Cart Items */}
-        <div className="space-y-4">
-        {items && items.length === 0 ? (
-        <div className="flex flex-col items-center justify-center rounded h-[500px] bg-gradient-to-br from-green-100 to-purple-200">
-          <Image
-            src={"/empty-cart.svg"}
-            alt="Empty Cart"
-            width={300}
-            height={300}
-          />
-          <h2 className="text-2xl font-bold text-gray-700 mt-4 font-Poppins">
-            Your Cart is Empty
-          </h2>
-          <p className="text-gray-500 mt-2">
-            Looks like you haven’t added anything to your cart yet.
-          </p>
-          <Link
-            href={"/shop/all-products"}
-            className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md"
-          >
-            Continue Shopping
-          </Link>
-        </div>
-      ) : (
-        <div className="grid grid-cols-1 gap-5 py-3">
-          <div className="col-span-1 sm:col-span-2 space-y-3">
-            {items.map((item) => (
-              <div
-                key={item.id}
-                className="flex justify-between  relative bg-gray-200 rounded  items-start sm:items-center px-3 py-2"
+        <div className="space-y-4 h-[calc(100%-42px)]">
+          {items && items.length === 0 ? (
+            <div className="flex flex-col items-center justify-center rounded h-[500px] bg-gradient-to-br from-green-100 to-purple-200">
+              <Image
+                src={"/empty-cart.svg"}
+                alt="Empty Cart"
+                width={300}
+                height={300}
+              />
+              <h2 className="text-2xl font-bold text-gray-700 mt-4 font-Poppins">
+                Your Cart is Empty
+              </h2>
+              <p className="text-gray-500 mt-2">
+                Looks like you haven’t added anything to your cart yet.
+              </p>
+              <Link
+                href={"/shop/all-products"}
+                className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md"
               >
-                <div
-                  onClick={() => removeItemFromCart(item.id)}
-                  className="absolute  -top-2 -right-1 size-7 bg-white border hover:bg-red-100 cursor-pointer rounded-full flex justify-center items-center"
-                >
-                  <IoClose size={25} color="red" />
-                </div>
+                Continue Shopping
+              </Link>
+            </div>
+          ) : (
+            <div className="flex flex-col h-[calc(100%-42px)]  gap-5 py-3">
+              <div className="flex-1 overflow-y-auto">
+                {items.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex justify-between mb-3  relative bg-gray-200 rounded  items-start sm:items-center px-3 py-2"
+                  >
+                    <div
+                      onClick={() => removeItemFromCart(item.id)}
+                      className="absolute  top-1/2  -translate-y-1/2 right-1 size-7 bg-white border hover:bg-red-100 cursor-pointer rounded-full flex justify-center items-center"
+                    >
+                      <MdDeleteForever size={20} color="red" />
+                    </div>
 
-                <div className="flex basis-full sm:basis-2/5 sm:items-center gap-x-3 ">
-                  {item?.image && (
-                    <div className="w-auto">
-                      <Image
-                        src={item?.image}
-                        alt={item.title}
-                        width={120}
-                        height={100}
-                        className="rounded w-auto h-[100px] object-cover"
-                      />
+                    <div className="flex basis-full  sm:items-center gap-x-3 ">
+                      {item?.image && (
+                        <div className="w-auto">
+                          <Image
+                            src={item?.image}
+                            alt={item.title}
+                            width={120}
+                            height={100}
+                            className="rounded w-auto h-[100px] object-cover"
+                          />
+                        </div>
+                      )}
+                      <div>
+                        <h4 className="capitalize text-[14px]">{item.title}</h4>
+                        <p className="text-gray-600 text-[12px]">
+                          Aed {item.price}
+                        </p>
+                        <div className="flex gap-x-3">
+                          {item?.selectedSize && (
+                            <>
+                              <p className="text-gray-600 text-[12px]">
+                                {item?.selectedSize}
+                              </p>
+                              <div className="h-4 w-[1px] bg-black transition"></div>
+                            </>
+                          )}
+                          {item?.selectedColor && (
+                            <p className="text-gray-600 text-[12px]">
+                              {item?.selectedColor}
+                            </p>
+                          )}
+                          {item?.selectedFeature && (
+                            <>
+                              <div className="h-4 w-[1px] bg-black transition"></div>
+                              <p className="text-gray-600 text-[12px]">
+                                {item?.selectedFeature}
+                              </p>
+                            </>
+                          )}
+                          <div className="border my-2 border-black bg-white rounded-sm basis-full sm:hidden px-3 flex items-center justify-between">
+                            <div>
+                              <button onClick={() => decreaseQuantity(item.id)}>
+                                <FaMinus
+                                  size={10}
+                                  className="hover:text-red-500"
+                                />
+                              </button>
+                            </div>
+                            <div>{item.quantity}</div>
+                            <div>
+                              <button onClick={() => increaseQuantity(item.id)}>
+                                <FaPlus
+                                  size={10}
+                                  className="hover:text-green-500"
+                                />
+                              </button>
+                            </div>
+                          </div>
+
+                          <div className="sm:hidden">
+                            <p>Subtotal: Aed {item.price * item.quantity}</p>
+                          </div>
+                        </div>
+                        <div className="border my-3 rounded-sm bg-white border-black  w-40 px-3 flex items-center justify-between">
+                          <div>
+                            <button onClick={() => decreaseQuantity(item.id)}>
+                              <FaMinus
+                                size={10}
+                                className="hover:text-red-500"
+                              />
+                            </button>
+                          </div>
+                          <div>{item.quantity}</div>
+                          <div>
+                            <button onClick={() => increaseQuantity(item.id)}>
+                              <FaPlus
+                                size={10}
+                                className="hover:text-green-500"
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="flex text-[14px]">
+                          <p>Subtotal: Aed {item.price * item.quantity}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="h-48">
+                <div className="p-3 ">
+                  <div className="flex justify-between">
+                    <div>
+                      Total (
+                      {items?.length > 0 &&
+                        items?.reduce((prev, cur) => {
+                          return prev + cur.quantity;
+                        }, 0)}
+                      ) Items
+                    </div>
+                    <div>Aed {totalAmount}</div>
+                  </div>
+                  {totalAmount > 100 ? (
+                    <div className="flex my-2 justify-between">
+                      <div>Shipping: </div>
+                      <div>Free</div>
+                    </div>
+                  ) : (
+                    <div className="flex my-4 justify-between">
+                      <div>Standard Shipping: </div>
+                      <div>Aed {process.env.NEXT_PUBLIC_SHIPPING_CHARGES}</div>
                     </div>
                   )}
-                  <div>
-                    <h4 className="capitalize text-[14px]">{item.title}</h4>
-                    <p className="text-gray-600 text-[12px]">
-                      Aed {item.price}
-                    </p>
-                    <div className="flex gap-x-3">
-                      {item?.selectedSize && (
-                        <>
-                          <p className="text-gray-600 text-[12px]">
-                            {item?.selectedSize}
-                          </p>
-                          <div className="h-4 w-[1px] bg-black transition"></div>
-                        </>
-                      )}
-                      {item?.selectedColor && (
-                        <p className="text-gray-600 text-[12px]">
-                          {item?.selectedColor}
-                        </p>
-                      )}
-                      {item?.selectedFeature && (
-                        <>
-                          <div className="h-4 w-[1px] bg-black transition"></div>
-                          <p className="text-gray-600 text-[12px]">
-                            {item?.selectedFeature}
-                          </p>
-                        </>
-                      )}
-                    </div>
-                    <div className="border my-2 border-black bg-white rounded-sm basis-full sm:hidden px-3 flex items-center justify-between">
-                      <div>
-                        <button onClick={() => decreaseQuantity(item.id)}>
-                          <FaMinus size={10} className="hover:text-red-500" />
-                        </button>
-                      </div>
-                      <div>{item.quantity}</div>
-                      <div>
-                        <button onClick={() => increaseQuantity(item.id)}>
-                          <FaPlus size={10} className="hover:text-green-500" />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="sm:hidden">
-                      <p>Subtotal: Aed {item.price * item.quantity}</p>
+                  <hr />
+                  <div className="flex mt-4 justify-between">
+                    <div>Total Amount:</div>
+                    <div className="text-gray-500 font-bold">
+                      Aed{" "}
+                      {totalAmount > 100
+                        ? totalAmount
+                        : totalAmount +
+                          parseInt(process.env.NEXT_PUBLIC_SHIPPING_CHARGES!)}
                     </div>
                   </div>
                 </div>
-
-                <div className="border rounded-sm bg-white border-black hidden sm:basis-2/12 px-3 sm:flex items-center justify-between">
-                  <div>
-                    <button onClick={() => decreaseQuantity(item.id)}>
-                      <FaMinus size={10} className="hover:text-red-500" />
-                    </button>
-                  </div>
-                  <div>{item.quantity}</div>
-                  <div>
-                    <button onClick={() => increaseQuantity(item.id)}>
-                      <FaPlus size={10} className="hover:text-green-500" />
-                    </button>
-                  </div>
-                </div>
-
-                <div className="hidden sm:flex">
-                  <p>Subtotal: Aed {item.price * item.quantity}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-          {/* <div className="col-span-1 bg-gray-100 p-5 rounded">
-            <h1 className="text-3xl">Summary</h1>
-
-            <hr className="bg-black" />
-
-            <div className="flex my-6 justify-between">
-              <div>Total Items:</div>
-              <div>
-                {items?.length > 0 &&
-                  items?.reduce((prev, cur) => {
-                    return prev + cur.quantity;
-                  }, 0)}{" "}
-                Items
+                <button
+                  onClick={handleCheckout}
+                  className="bg-black flex gap-x-2 items-center justify-center w-full py-2 text-lg capitalize font-Poppins hover:bg-white hover:text-primary hover:border-2 border-primary  rounded text-white mt-1"
+                >
+                  Checkout <FaArrowRight />
+                </button>
+                <div className="flex justify-center mt-2"><img src="/drawer-payment.png" className="w-2/3 " alt="payment-methods"/></div>
               </div>
             </div>
-            <div className="bg-white rounded p-3">
-              <div className="flex justify-between">
-                <div>Total Amount:</div>
-                <div>Aed {totalAmount}</div>
-              </div>
-              {totalAmount > 100 ? (
-                <div className="flex my-4 justify-between">
-                  <div>Shipping: </div>
-                  <div>Free</div>
-                </div>
-              ) : (
-                <div className="flex my-4 justify-between">
-                  <div>Standard Shipping: </div>
-                  <div>Aed {process.env.NEXT_PUBLIC_SHIPPING_CHARGES}</div>
-                </div>
-              )}
-              <hr />
-              <div className="flex mt-4 justify-between">
-                <div>Total Price:</div>
-                <div className="text-gray-500 font-bold">
-                  Aed{" "}
-                  {totalAmount > 100
-                    ? totalAmount
-                    : totalAmount +
-                      parseInt(process.env.NEXT_PUBLIC_SHIPPING_CHARGES!)}
-                </div>
-              </div>
-            </div>
-            <button
-              onClick={handleCheckout}
-              className="bg-primary w-full py-3 text-lg capitalize font-Poppins hover:bg-white hover:text-primary hover:border-2 border-primary  rounded text-white mt-10"
-            >
-              Continue
-            </button>
-          </div> */}
+          )}
         </div>
-      )}
-        </div>
-
-        {/* Checkout Button */}
-        <button className="w-full mt-6 bg-black text-white py-2 rounded-md">
-          Checkout
-        </button>
       </div>
     </>
   );
