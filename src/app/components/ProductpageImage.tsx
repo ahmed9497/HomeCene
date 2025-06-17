c840b72df0ec0fb082fd48ee0c89bc1329460ddc
Ahmed Khalil <ahmedkhalil9497@yahoo.com>
2025-06-17 14:47:53 +0500

    video

--- src/app/components/ProductpageImage.tsx
+++ src/app/components/ProductpageImage.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState } from 'react'
+import { FaPlay, FaRegCirclePlay, FaVideo } from 'react-icons/fa6';
 
 const ProductPageImage = ({product}:{product:any}) => {
     const [activeImg,setActiveImg] = useState(0);
@@ -18,6 +19,17 @@ const ProductPageImage = ({product}:{product:any}) => {
 
    { product?.images?.length > 0 &&
     <div className="max-h-[610px] flex justify-center items-center">
+      {product?.images[activeImg]?.endsWith(".mp4") ||
+            product?.images[activeImg]?.endsWith(".webm") ? (
+              <video
+                src={product?.images[activeImg]}
+                controls
+                // preload='none'
+                autoPlay
+                poster={product?.images[0]}
+                className="max-h-[600px] object-contain h-auto sm:h-[600px] rounded-md"
+              />
+            ) : (
       <img
         src={
           product?.images?.length ? product?.images[activeImg] : "/chair1.webp"
@@ -25,16 +37,46 @@ const ProductPageImage = ({product}:{product:any}) => {
         alt={product?.title}
         className="sm:object-cover max-h-[600px] object-contain h-auto sm:h-[600px] rounded-md"
       />
+      )
+}
     </div>
 }
       </div>
     <div className="flex justify-center flex-wrap gap-x-4 mt-3">
       {product?.images?.length >0 &&
-        product.images.map((i: any,index:number) => (
-          <div key={index} className={`w-20 border  h-[80px] p-1 rounded-sm bg-imgBg cursor-pointer ${activeImg === index && 'border-primary'}`} onClick={()=>setActiveImg(index)}>
+        product.images.map((i: any,index:number) => {
+          const isVideo = i.endsWith(".mp4") || i.endsWith(".webm");
+
+          return isVideo ? (
+            <div
+              key={index}
+              className={`w-20 border relative h-[80px] p-1 rounded-sm bg-imgBg cursor-pointer ${
+                activeImg === index ? "border-primary" :"opacity-50"
+              }`}
+              onClick={() => setActiveImg(index)}
+            >
+              <div className='absolute left-1/2 top-1/2 -translate-x-1/2 size-[40px] flex place-content-center place-items-center -translate-y-1/2 bg-white rounded-full'>
+                <FaPlay size={20} />
+              </div>
+              <video
+                key={index}
+                muted
+                // autoPlay
+                poster={product?.images[0]}
+                preload='none'
+                className="w-full h-auto rounded-lg"
+              >
+                <source src={product?.images[index]} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          ) : (
+          <div key={index} className={`w-20 border  h-[80px] p-1 rounded-sm bg-imgBg cursor-pointer ${activeImg === index ? 'border-primary' :'opacity-50'}`} onClick={()=>setActiveImg(index)}>
             <img src={product?.images[index]} className="object-cover w-full h-full rounded-sm" alt={product.title}/>
           </div>
-        ))}
+          )
+        }
+        )}
     </div>
   </div>
   )
